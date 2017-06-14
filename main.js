a8a712d1b65aac0a81a4a690634675510d875f2b
Daniel Duffield <danieljduffield@gmail.com>
2017-06-14 10:59:35 -0700

    Split selectTile function into two functions with getSelectedTiles. #2

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function generateTiles() {
-  var tiles = []
+  let tiles = []
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      var tile = {}
-      tile.originRow = i
-      tile.originCol = j
-      tile.row = i
-      tile.column = j
-      tile.isSelected = false
-      tile.isHidden = false
+      let tile = {
+        originRow: i,
+        originCol: j,
+        row: i,
+        column: j,
+        isSelected: false,
+        isHidden: false
+      }
       if (tile.row === 0 || tile.row === 7 || tile.column === 0 || tile.column === 7) {
         tile.isHidden = true
       }
@@ -19,9 +20,9 @@ function generateTiles() {
 }
 
 function generateBoard(tiles) {
-  var row = []
-  var board = []
-  var rows = 8
+  let row = []
+  let board = []
+  let rows = 8
   for (let i = 0; i < rows; i++) {
     row = tiles.splice(0, 8)
     board.push(row)
@@ -30,17 +31,17 @@ function generateBoard(tiles) {
 }
 
 function renderBoard(board) {
-  var $board = document.createElement('div')
+  let $board = document.createElement('div')
   $board.setAttribute('id', 'board-render')
   for (let i = 0; i < board.length; i++) {
-    var $row = renderRow(board[i])
+    let $row = renderRow(board[i])
     $board.appendChild($row)
   }
   return $board
 }
 
 function renderTile(tile) {
-  var $tile = document.createElement('div')
+  let $tile = document.createElement('div')
   $tile.setAttribute('class', 'board-tile')
   if (tile.isHidden === true) {
     $tile.classList.add('hidden-tile')
@@ -54,18 +55,18 @@ function renderTile(tile) {
 }
 
 function renderRow(tiles) {
-  var $row = document.createElement('div')
+  let $row = document.createElement('div')
   $row.setAttribute('class', 'board-row')
   $row.setAttribute('id', 'row-' + tiles[0].row)
   for (let i = 0; i < tiles.length; i++) {
-    var $tile = renderTile(tiles[i])
+    let $tile = renderTile(tiles[i])
     $row.appendChild($tile)
   }
   return $row
 }
 
 function updateBoard(board) {
-  var $board = document.getElementById('board-render')
+  let $board = document.getElementById('board-render')
   $board.removeEventListener('click', selectTile)
   document.getElementById('game-board').removeChild($board)
   $board = renderBoard(board)
@@ -73,8 +74,8 @@ function updateBoard(board) {
   $board.addEventListener('click', selectTile)
 }
 
-function swapTiles(board) {
-  var selectedTiles = []
+function getSelectedTiles(board) {
+  let selectedTiles = []
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (board[i][j].isSelected === true) {
@@ -82,39 +83,48 @@ function swapTiles(board) {
       }
     }
   }
-  if (selectedTiles.length > 1) {
-    let i = selectedTiles[0]
-    let j = selectedTiles[1]
-    let tempColumn = i.originCol
-    let tempRow = i.originRow
-    i.originRow = j.originRow
-    i.originCol = j.originCol
-    j.originRow = tempRow
-    j.originCol = tempColumn
-    i.isSelected = false
-    j.isSelected = false
-  }
+  return selectedTiles
 }
 
-var board = generateBoard(generateTiles())
-var $board = renderBoard(board)
-var $start = document.getElementById('start-button')
-var $container = document.getElementById('container')
+function swapTiles(selectedTiles) {
+  let i = selectedTiles[0]
+  let j = selectedTiles[1]
+  console.log('swap')
+  console.log(i)
+  console.log(j)
+  let tempColumn = i.originCol
+  let tempRow = i.originRow
+  i.originRow = j.originRow
+  i.originCol = j.originCol
+  j.originRow = tempRow
+  j.originCol = tempColumn
+  i.isSelected = false
+  j.isSelected = false
+}
 
-var startGame = function(event) {
-  console.log('clicked start')
+let board = generateBoard(generateTiles())
+let $board = renderBoard(board)
+let $start = document.getElementById('start-button')
+let $container = document.getElementById('container')
+
+let startGame = function(event) {
   document.getElementById('game-board').appendChild($board)
   removeEventListener('click', startGame)
   $container.removeChild($start)
 }
 
-var selectTile = function(event) {
+let selectTile = function(event) {
   if (!(event.target.classList[1] === 'hidden-tile') && event.target.classList[0] === 'board-tile') {
-    console.log(event.target)
-    var $current = board[event.target.id[4]][event.target.id[13]]
+    let $current = {}
+    $current = board[event.target.id[4]][event.target.id[13]]
     $current.isSelected = !$current.isSelected
-    console.log($current)
-    swapTiles(board)
+    if ($current.isSelected === false) {
+      $current = 0
+    }
+    let selectedTiles = getSelectedTiles(board)
+    if (selectedTiles.length > 1) {
+      swapTiles(selectedTiles)
+    }
     updateBoard(board)
   }
 }

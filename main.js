3a630815d969ef1e757044da64a71b0983492591
Daniel Duffield <danieljduffield@gmail.com>
2017-06-22 11:12:38 -0700

    Added a condition on game loss that keeps charge in phase 4 to keep it red instead of aqua. #9

--- main.js
+++ main.js
@@ -480,6 +480,12 @@ function animateCharge(timer) {
   let $tileImage = document.getElementById('row-' + game.chargeCoordinates[0] + ' column-' + game.chargeCoordinates[1] + ' image')
   let $tile = document.getElementById('row-' + game.chargeCoordinates[0] + ' column-' + game.chargeCoordinates[1])
   let tile = game.board[game.chargeCoordinates[0]][game.chargeCoordinates[1]]
+  if (game.loss === true) {
+    $tileImage.classList.remove('charge-phase-0')
+    $tile.classList.remove('charge-phase-0')
+    $tileImage.classList.add('charge-phase-4')
+    $tile.classList.add('charge-phase-4')
+  }
   if ($tileImage && hasClass($tileImage, 'charged')) {
     if (hasClass($tileImage, ('charge-phase-' + (timer - 1)))) {
       $tileImage.classList.remove('charge-phase-' + timer - 1)
@@ -524,7 +530,6 @@ let restartGame = function() {
 }
 
 function startTimer() {
-  console.log(game.timer)
   let $timerText = document.getElementById('timer-text')
   let $countdown = document.getElementById('countdown')
   let $restart = document.createElement('button')

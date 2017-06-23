85e5164e00698637880c805a98223105fa8298e9
Daniel Duffield <danieljduffield@gmail.com>
2017-06-23 14:28:34 -0700

    Created a function to highlight title letters in sequence from right to left. #26

--- main.js
+++ main.js
@@ -784,6 +784,39 @@ function animateTitleForward() {
   cycles++
 }
 
+function animateTitleBackward() {
+  if (cycles < 1) {
+    let $lastLetter = document.getElementById('span-0')
+    if ($lastLetter.classList.length) {
+      $lastLetter.classList.remove('charged')
+    }
+    return
+  }
+  window.setTimeout(animateTitleBackward, 100)
+  if (letterIndex === 20) {
+    let $lastLetter = document.getElementById('span-0')
+    if ($lastLetter.classList.length) {
+      $lastLetter.classList.remove('charged')
+    }
+  }
+  if (letterIndex + 1 <= 20) {
+    let $lastLetter = document.getElementById('span-' + (letterIndex + 1))
+    $lastLetter.classList.remove('charged')
+  }
+  let $currentLetter = document.getElementById('span-' + letterIndex)
+  $currentLetter.classList.add('charged')
+  letterIndex--
+  if (letterIndex === -1) {
+    if (cycles === 1) {
+      letterIndex = 0
+    }
+    else {
+      letterIndex = 20
+    }
+  }
+  cycles--
+}
+
 let letterIndex = 0
 let cycles = 0
 
